bfd8172478bb63b79bfdefe9798ba66f42c6fa84
KabanFriends <yuukun2004@protonmail.ch>
2024-03-05 21:04:13 +0900

    Fix damage tracking not working correctly when the damage event is cancelled

--- core/src/main/java/io/github/kabanfriends/kabansmp/module/damage/event/DamageEventHandler.java
+++ core/src/main/java/io/github/kabanfriends/kabansmp/module/damage/event/DamageEventHandler.java
@@ -4,13 +4,17 @@ import io.github.kabanfriends.kabansmp.module.damage.DamageModule;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class DamageEventHandler implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerTakeDamage(EntityDamageEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
         if (!(event.getEntity() instanceof Player player)) {
             return;
         }
